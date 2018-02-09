fb5783799feaec23993871a6355967c537bd637c
Juji <him@jujiyangasli.com>
2018-02-10 03:57:46 +0700

    dev14

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name        : "circle",
     script      : "npm",
-    args        : "start"
+    args        : "start",
     watch       : true,
     env: {
       "NODE_ENV": "development",
